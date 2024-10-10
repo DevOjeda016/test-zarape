add9d8f83fb8fe3f6362f5fdf249831838a01b3c
Ojeda 16 <172947644+DevOjeda016@users.noreply.github.com>
2024-10-09 20:54:01 -0600

    upload login-test

---
+++ tests/login.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('Mensaje de fallo de autenticacion', async ({ page }) => {
+  await page.goto('https://elzarape.github.io/pages/login.html');
+  await page.getByLabel('Usuario').click();
+  await page.getByLabel('Usuario').fill('Admin.invitad'); //Usuario incorrecto
+  await page.getByLabel('Contraseña').click();
+  await page.getByLabel('Contraseña').fill('AdminInvitado16*');
+  await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+  await expect(page.getByText('Usuario o contraseña')).toBeVisible();
+});
+
+test('Inicio de sesion', async ({ page }) => {
+  await page.goto('https://elzarape.github.io/pages/login.html');
+  await page.getByLabel('Usuario').click();
+  await page.getByLabel('Usuario').fill('Admin.invitado');
+  await page.getByLabel('Contraseña').click();
+  await page.getByLabel('Contraseña').fill('AdminInvitado16*');
+  await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+  await expect(page).toHaveURL('https://elzarape.github.io/admin/panel.html');
+});
+
+
+
