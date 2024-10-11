4aa94b1e284d08d43adcaa70ecfc008df0ec25f0
Ojeda 16 <172947644+DevOjeda016@users.noreply.github.com>
2024-10-11 08:05:14 -0600

    update method busqueda usuarios

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,8 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Busqueda usuarios', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://elzarape.github.io/admin/modules/users/view/user.html');
+});
+
+test('Busqueda usuarios', async ({ page }) => {
   await page.getByRole('button').nth(1).click();
   await page.getByLabel('Usuario:').click();
   await page.getByLabel('Usuario:').fill('Admin');
 });
+
+/* 
+test('Validacion de campos'), async ({ page }) => {
+  await 
+} */
