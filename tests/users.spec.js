0badde91f21d9a17a357e949f21b637426e790a8
DevOjeda016 <172947644+DevOjeda016@users.noreply.github.com>
2024-10-14 00:07:11 -0600

    Delete method duplicated

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -10,6 +10,65 @@ test('Busqueda usuarios', async ({ page }) => {
   await page.getByLabel('Usuario:').fill('Admin');
 });
 
+test.describe('Automatizacion CRUD', () => {
+  test('Crear', async ({ page }) => {
+    await page.getByRole('button', { name: 'Crear nuevo registro' }).click();
+    await page.getByLabel('Nombre(s)').fill('Daniel');
+    const nombre = await page.getByLabel('Nombre(s)').inputValue();
+    await page.getByText('Apellido paterno').fill('Ojeda');
+    const aPaterno = await page.getByText('Apellido paterno').inputValue();
+    await page.getByText('Apellido materno').fill('Luna');
+    const aMaterno = await page.getByText('Apellido materno').inputValue();
+    await page.getByLabel('Telefono').fill('4776472735');
+    await page.getByLabel('Sucursal').selectOption('3');
+    await page.getByLabel('Usuario', { exact: true }).fill('AdminInvitadoTest');
+    await page.getByRole('button', { name: 'Generar contraseña' }).click();
+    await page.getByRole('button', { name: 'Registrar' }).click();
+    await expect(page.getByLabel(`Vista previa ${nombre} ${aPaterno} ${aMaterno}`)).toBeVisible();
+    await page.getByRole('button', { name: 'Sí' }).click();
+  })
+  
+  test('Actualizar', async ({ page }) => {
+    await page.getByRole('cell', { name: 'Admin', exact: true }).click();
+    await page.getByLabel('Nombre(s)').fill('Daniel');
+    const nombre = await page.getByLabel('Nombre(s)').inputValue();
+    await page.getByText('Apellido paterno').fill('Ojeda');
+    const aPaterno = await page.getByText('Apellido paterno').inputValue();
+    await page.getByText('Apellido materno').fill('Luna');
+    const aMaterno = await page.getByText('Apellido materno').inputValue();
+    await page.getByLabel('Telefono').fill('4776472735');
+    await page.getByLabel('Sucursal').selectOption('3');
+    await page.getByLabel('Estatus').selectOption('2');
+    await page.getByRole('button', { name: 'Modificar constraseña' }).click();
+    await page.getByLabel('Usuario', { exact: true }).click();
+    await page.getByRole('button', { name: 'Generar contraseña' }).click();
+    await page.getByRole('button', { name: 'Modificar' }).click();
+    await expect(page.getByLabel(`Vista previa ${nombre} ${aPaterno} ${aMaterno}`)).toBeVisible();
+    await page.getByRole('button', { name: 'Sí' }).click();
+  })
+
+  test('Eliminar', async ( {page} ) => {
+    await page.getByRole('cell', { name: 'Admin', exact: true}).click();
+    await page.getByRole('button', { name: 'Eliminar' }).click();
+    await expect(page.getByLabel('Vista previa Admin Invitado Temporal')).toBeVisible();
+    await page.getByRole('button', { name: 'Sí' }).click();
+  })
+
+  test('Limpiar', async ( {page} ) => {
+    await page.getByRole('cell', { name: 'Admin', exact: true}).click();
+    await page.getByRole('button', { name: 'Limpiar' }).click();
+    await expect(page.getByLabel('Nombre(s)')).toHaveValue('');
+    await expect(page.getByText('Apellido paterno')).toHaveValue('');
+    await expect(page.getByText('Apellido materno')).toHaveValue('');
+    await expect(page.getByLabel('Sucursal')).toHaveValue('');
+    await expect(page.getByLabel('Usuario', { exact: true })).toHaveValue('');
+    await page.getByRole('button', { name: 'Modificar constraseña' }).click();
+    await expect(page.getByLabel('Nueva contraseña', { exact: true })).toHaveValue('');
+    await expect(page.getByLabel('Confirmar Nueva contraseña')).toHaveValue('');
+    await expect(page.getByLabel('Estatus')).toHaveValue('');
+  })
+})
+
 
 test.describe('Mensajes de validacion de campos de nuevo registro', () => {
   test.beforeEach(async ({ page }) => {
@@ -81,8 +140,8 @@ test.describe('Mensajes de validacion de campos de actualizar registro', () => {
   test.beforeEach(async ({ page }) => {
     await page.getByRole('cell', { name: 'Admin', exact: true }).click();
     await page.getByLabel('Nombre(s)').fill('');
-    page.getByText('Apellido paterno').fill('');
-    page.getByText('Apellido materno').fill('');
+    await page.getByText('Apellido paterno').fill('');
+    await page.getByText('Apellido materno').fill('');
     await page.getByLabel('Telefono').fill('');
     await page.getByLabel('Sucursal').selectOption('');
     await page.getByLabel('Usuario', { exact: true }).fill('');
